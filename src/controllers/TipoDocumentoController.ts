477fd8341788a79ed4492379f949a2908c6506ad
elias <eliasflores13@outlook.com>
2020-03-16 23:19:52 -0700

    Modified the tiposDocumentosController to send status codes on the responses

--- src/controllers/TipoDocumentoController.ts
+++ src/controllers/TipoDocumentoController.ts
@@ -6,7 +6,7 @@ export class TipoDocumentoController {
     private repository = getRepository(TipoDocumento)
 
     async all(request: Request, response: Response, next: NextFunction) {
-        return response.send(this.repository.find())
+        return response.status(200).send(this.repository.find())
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
@@ -14,15 +14,23 @@ export class TipoDocumentoController {
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
-        return this.repository.save(request.body)
+        return response.status(201).send(this.repository.save(request.body))
     }
 
     async update(request: Request, response: Response, next: NextFunction) {
-        return this.repository.update(request.params.id, request.body)
+        const id = request.params.id
+        const tipoDocumento = await this.repository.findOne(id)
+        if (tipoDocumento !== undefined) {
+            const updatedRecord = await this.repository.update(request.params.id, request.body)
+            return response.status(200).send(updatedRecord)
+        }
+        return response.status(404).send({message: "Record not found"})
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
         const recordToRemove = await this.repository.findOne(request.params.id)
-        return this.repository.remove(recordToRemove)
+        await this.repository.remove(recordToRemove)
+
+        return response.status(200).send({message: "Record deleted successfully"})
     }
 }
